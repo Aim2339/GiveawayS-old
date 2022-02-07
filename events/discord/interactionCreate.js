b81957aea7973a78e76df12b40f2f84299196a5e
Aim <91464959+Aim2339@users.noreply.github.com>
2022-02-07 18:20:47 +0530

    Format using Prettier

--- events/discord/interactionCreate.js
+++ events/discord/interactionCreate.js
@@ -1,15 +1,13 @@
 module.exports = (client, interaction) => {
-
-    if (interaction.isCommand()) {
-
+  if (interaction.isCommand()) {
     const command = client.interactions.get(interaction.commandName);
 
-
-    if (!command) return interaction.reply({
-      content: ":x: Something Went Wrong | Perhaps command not registered?",
-      ephemeral: true
-    });
+    if (!command)
+      return interaction.reply({
+        content: ":x: Something Went Wrong | Perhaps command not registered?",
+        ephemeral: true,
+      });
 
     command.run(client, interaction);
   }
-}
+};
