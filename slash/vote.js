e0ae64c57cc41c2c017ff95b482699344a5f9d24
Aim <91464959+Aim2339@users.noreply.github.com>
2022-02-08 17:04:28 +0530

    Format using Prettier

--- slash/vote.js
+++ slash/vote.js
@@ -1,23 +1,27 @@
-const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 
 module.exports = {
-  name: 'vote',
-  description: '➕ Vote for the bot!',
+  name: "vote",
+  description: "➕ Vote for the bot!",
   run: async (client, interaction) => {
-    const row = new MessageActionRow()
-      .addComponents(
-        new MessageButton()
-          .setLabel(`Vote for ${client.user.username}`)
-          .setStyle('LINK')
-          .setURL(`https://top.gg/bot/${client.user.id}/vote`),
-      )
+    const row = new MessageActionRow().addComponents(
+      new MessageButton()
+        .setLabel(`Vote for ${client.user.username}`)
+        .setStyle("LINK")
+        .setURL(`https://top.gg/bot/${client.user.id}/vote`)
+    );
     let invite = new MessageEmbed()
       .setAuthor(`Vote on Top.gg`, client.user.avatarURL())
       .setTitle("Vote Link!")
-      .setDescription(`Every vote counts, if you like ${client.user} then help us by voting!`)
-      .setColor('RANDOM')
+      .setDescription(
+        `Every vote counts, if you like ${client.user} then help us by voting!`
+      )
+      .setColor("RANDOM")
       .setTimestamp()
-      .setFooter(`Requested by ${interaction.user.username} | GiveawaySforLife`, interaction.user.displayAvatarURL())
+      .setFooter(
+        `Requested by ${interaction.user.username} | GiveawaySforLife`,
+        interaction.user.displayAvatarURL()
+      );
     interaction.reply({ embeds: [invite], components: [row] });
-  }
-}
+  },
+};
