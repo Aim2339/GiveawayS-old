3cae0baae07a585b3498990ad6eb5500f25e50b2
Aim <91464959+Aim2339@users.noreply.github.com>
2022-02-08 10:06:35 +0530

    Format using Prettier + Support for Logs

--- slash/create.js
+++ slash/create.js
@@ -1,196 +1,223 @@
-const Discord = require("discord.js")
-const { MessageEmbed } = require("discord.js")
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 const messages = require("../utils/message");
-const ms = require("ms")
+const ms = require("ms");
 module.exports = {
-  name: 'create',
-  description: '🎉 Create a giveaway',
+  name: "create",
+  description: "🎉 Create a giveaway",
 
   options: [
     {
-      name: 'duration',
-      description: 'How long the giveaway should last for. Example values: 1m, 1h, 1d',
-      type: 'STRING',
-      required: true
+      name: "duration",
+      description:
+        "How long the giveaway should last for. Example values: 1m, 1h, 1d",
+      type: "STRING",
+      required: true,
     },
     {
-      name: 'winners',
-      description: 'How many winners the giveaway should have',
-      type: 'INTEGER',
-      required: true
+      name: "winners",
+      description: "How many winners the giveaway should have",
+      type: "INTEGER",
+      required: true,
     },
     {
-      name: 'prize',
-      description: 'What the prize of the giveaway should be',
-      type: 'STRING',
-      required: true
+      name: "prize",
+      description: "What the prize of the giveaway should be",
+      type: "STRING",
+      required: true,
     },
     {
-      name: 'channel',
-      description: 'The channel to start the giveaway in',
-      type: 'CHANNEL',
-      required: true
+      name: "channel",
+      description: "The channel to start the giveaway in",
+      type: "CHANNEL",
+      required: true,
     },
     {
-      name: 'pingrole',
-      description: 'The role to ping when starting the giveaway',
-      type: 'ROLE',
-      required: false 
+      name: "pingrole",
+      description: "The role to ping when starting the giveaway",
+      type: "ROLE",
+      required: false,
     },
     {
-      name: 'bonusrole',
-      description: 'Role which would recieve bonus entries',
-      type: 'ROLE',
-      required: false
+      name: "bonusrole",
+      description: "Role which would recieve bonus entries",
+      type: "ROLE",
+      required: false,
     },
     {
-      name: 'bonusamount',
-      description: 'The amount of bonus entries the role will recieve',
-      type: 'INTEGER',
-      required: false
+      name: "bonusamount",
+      description: "The amount of bonus entries the role will recieve",
+      type: "INTEGER",
+      required: false,
     },
-        {
-      name: 'note',
-      description: 'Anything you wanna type (can include requirements/how to claim)',
-      type: 'STRING',
-      required: false
+    {
+      name: "note",
+      description:
+        "Anything you wanna type (can include requirements/how to claim)",
+      type: "STRING",
+      required: false,
     },
   ],
 
   run: async (client, interaction) => {
-
     // If the member doesn't have enough permissions
-    if (!interaction.member.permissions.has('MANAGE_MESSAGES') && !interaction.member.roles.cache.some((r) => r.name === "Giveaways")) {
+    if (
+      !interaction.member.permissions.has("MANAGE_MESSAGES") &&
+      !interaction.member.roles.cache.some((r) => r.name === "Giveaways")
+    ) {
       return interaction.reply({
-        content: ':x: You need to have the manage messages permissions to start giveaways.',
-        ephemeral: true
+        content:
+          ":x: You need to have the manage messages permissions to start giveaways.",
+        ephemeral: true,
       });
     }
 
-    const giveawayChannel = interaction.options.getChannel('channel');
-    const giveawayDuration = interaction.options.getString('duration');
-    const giveawayWinnerCount = interaction.options.getInteger('winners');
-    const giveawayPrize = interaction.options.getString('prize');
-    const giveawayPing = interaction.options.getRole('pingrole');
-    const bonusRole = interaction.options.getRole('bonusrole');
-    const bonusEntries = interaction.options.getInteger('bonusamount');
-    const giveawayNote = interaction.options.getString('note');
+    const giveawayChannel = interaction.options.getChannel("channel");
+    const giveawayDuration = interaction.options.getString("duration");
+    const giveawayWinnerCount = interaction.options.getInteger("winners");
+    const giveawayPrize = interaction.options.getString("prize");
+    const giveawayPing = interaction.options.getRole("pingrole");
+    const bonusRole = interaction.options.getRole("bonusrole");
+    const bonusEntries = interaction.options.getInteger("bonusamount");
+    const giveawayNote = interaction.options.getString("note");
+
+    const logChannel = interaction.guild.channels.cache.find(
+      (channel) => channel.name === "giveaway-log"
+    );
 
     if (!giveawayChannel.isText()) {
       return interaction.reply({
-        content: ':x: Please select a text channel!',
-        ephemeral: true
+        content: ":x: Please select a text channel!",
+        ephemeral: true,
       });
     }
-    if(isNaN(ms(giveawayDuration))) {
-    return interaction.reply({
-      content: ':x: Please select a valid duration!',
-      ephemeral: true
-    });
-  }
+    if (isNaN(ms(giveawayDuration))) {
+      return interaction.reply({
+        content: ":x: Please select a valid duration!",
+        ephemeral: true,
+      });
+    }
     if (giveawayWinnerCount < 1) {
       return interaction.reply({
-        content: ':x: Please select a valid winner count! greater or equal to one.',
-      })
+        content:
+          ":x: Please select a valid winner count! greater or equal to one.",
+      });
     }
-        if (giveawayChannel.isThread()) {
+    if (giveawayChannel.isThread()) {
       module.exports = (client) => {
-        client.on('threadCreate', (thread) => {
+        client.on("threadCreate", (thread) => {
           try {
-            thread.join()
+            thread.join();
           } catch (e) {
             console.log(e);
           }
-        })
-      }
+        });
+      };
     }
-        if (bonusRole) {
+    if (bonusRole) {
       if (!bonusEntries) {
         return interaction.reply({
           content: `:x: You must specify how many bonus entries would ${bonusRole} recieve!`,
-          ephemeral: true
+          ephemeral: true,
         });
       }
     }
 
     if (bonusRole) {
-      messages.inviteToParticipate = `**React with 🎉 to participate!**\n>>> **${bonusRole}** Has **${bonusEntries}** Extra Entries in this giveaway!`
+      messages.inviteToParticipate = `**React with 🎉 to participate!**\n>>> **${bonusRole}** Has **${bonusEntries}** Extra Entries in this giveaway!`;
     }
     if (!bonusRole) {
-      messages.inviteToParticipate = `**React with 🎉 to participate!**`
+      messages.inviteToParticipate = `**React with 🎉 to participate!**`;
     }
 
     const nembed = new MessageEmbed()
-    .setTitle('Note from the host')
-    .setColor('RANDOM')
-    .setTimestamp()
-    .addFields(
-      {
+      .setTitle("Note from the host")
+      .setColor("RANDOM")
+      .setTimestamp()
+      .addFields({
         name: `Note from ${interaction.user.username}!`,
-        value: `>>> ${giveawayNote}`
-      }
-    )
-    .setFooter(`Note from ${interaction.user.username} | GiveawaysforLife`, interaction.user.displayAvatarURL())
-	
-    let msg = await interaction.reply({ content: `**Is everything correct?**\n>>> >>> - Channel: ${giveawayChannel}\n>>> - Duration: ${giveawayDuration}\n>>> - Winners: ${giveawayWinnerCount}\n>>> - Prize: ${giveawayPrize}\n>>> - Role to ping: ${giveawayPing}\n>>> - Bonus role: ${bonusRole}\n>>> - Bonus amount: ${bonusEntries}\n>>> - Note: ${giveawayNote}`,
-                                       ephemeral: false, fetchReply: true });
-    await msg.react("✅").then(() => msg.react('❌'));
+        value: `>>> ${giveawayNote}`,
+      })
+      .setFooter(
+        `Note from ${interaction.user.username} | GiveawaysforLife`,
+        interaction.user.displayAvatarURL()
+      );
+
+    let msg = await interaction.reply({
+      content: `**Is everything correct?**\n>>> >>> - Channel: ${giveawayChannel}\n>>> - Duration: ${giveawayDuration}\n>>> - Winners: ${giveawayWinnerCount}\n>>> - Prize: ${giveawayPrize}\n>>> - Role to ping: ${giveawayPing}\n>>> - Bonus role: ${bonusRole}\n>>> - Bonus amount: ${bonusEntries}\n>>> - Note: ${giveawayNote}`,
+      ephemeral: false,
+      fetchReply: true,
+    });
+    await msg.react("✅").then(() => msg.react("❌"));
     const filter = (reaction, user) => {
-	return ['✅', '❌'].includes(reaction.emoji.name) && user.id === interaction.user.id;
-};
+      return (
+        ["✅", "❌"].includes(reaction.emoji.name) &&
+        user.id === interaction.user.id
+      );
+    };
 
-msg.awaitReactions({ filter, max: 1, time: 60000, errors: ['time'] })
-	.then(collected => {
-		const reaction = collected.first();
+    msg
+      .awaitReactions({ filter, max: 1, time: 60000, errors: ["time"] })
+      .then((collected) => {
+        const reaction = collected.first();
 
-		if (reaction.emoji.name === '✅') {
-			interaction.editReply(`Giveaway has started in ${giveawayChannel}`);
-                 client.giveawaysManager.start(giveawayChannel, {
-          // The giveaway duration
-          duration: ms(giveawayDuration),
-          // The giveaway prize
-          prize: giveawayPrize,
-          // The giveaway winner count
-          winnerCount: parseInt(giveawayWinnerCount),
-          // The giveaway host
-          hostedBy: `<@!${interaction.user.id}>`,
-          // BonusEntries If Provided
-      bonusEntries: [
-        {
-          // Members who have the role which is assigned to "rolename" get the amount of bonus entries which are assigned to "BonusEntries"
-          bonus: new Function('member', `return member.roles.cache.some((r) => r.name === \'${bonusRole ?.name}\') ? ${bonusEntries} : null`),
-          cumulative: false
+        if (reaction.emoji.name === "✅") {
+          interaction.editReply(`Giveaway has started in ${giveawayChannel}`);
+          client.giveawaysManager.start(giveawayChannel, {
+            // The giveaway duration
+            duration: ms(giveawayDuration),
+            // The giveaway prize
+            prize: giveawayPrize,
+            // The giveaway winner count
+            winnerCount: parseInt(giveawayWinnerCount),
+            // The giveaway host
+            hostedBy: `<@!${interaction.user.id}>`,
+            // BonusEntries If Provided
+            bonusEntries: [
+              {
+                // Members who have the role which is assigned to "rolename" get the amount of bonus entries which are assigned to "BonusEntries"
+                bonus: new Function(
+                  "member",
+                  `return member.roles.cache.some((r) => r.name === \'${bonusRole?.name}\') ? ${bonusEntries} : null`
+                ),
+                cumulative: false,
+              },
+            ],
+            messages,
+          });
+          msg.reactions.removeAll();
+
+          if (logChannel) {
+            logChannel.send(
+              `**Giveaway started!**\n>>> ${interaction.user.username} started a giveaway of **${giveawayPrize}** in ${giveawayChannel}!`
+            );
+          } else {
+            return;
+          }
+
+          if (giveawayNote && giveawayPing) {
+            giveawayChannel.send({ embeds: [nembed] });
+            giveawayChannel.send(`${giveawayPing}`);
+          }
+          if (giveawayNote && !giveawayPing) {
+            giveawayChannel.send({ embeds: [nembed] });
+          }
+          if (!giveawayNote && giveawayPing) {
+            giveawayChannel.send(`${giveawayPing}`);
+          }
+          if (!giveawayNote && !giveawayPing) {
+            return;
+          }
+        } else {
+          interaction.editReply(":x: Giveaway was cancelled");
+          msg.reactions.removeAll();
         }
-      ],
-          messages,
-      
-        });
-      msg.reactions.removeAll() 
-
-    if (giveawayNote && giveawayPing) {
-      giveawayChannel.send({ embeds: [nembed] })
-      giveawayChannel.send(`${giveawayPing}`)
-    }
-    if (giveawayNote && !giveawayPing) {
-      giveawayChannel.send({ embeds: [nembed] });
-    } 
-    if (!giveawayNote && giveawayPing) {
-        giveawayChannel.send(`${giveawayPing}`);
-    }
-    if (!giveawayNote && !giveawayPing) {
-      return
-    }
-      
-		} else {
-			interaction.editReply(':x: Giveaway was cancelled');
-      msg.reactions.removeAll()
-		}
-
-	})
-	.catch(collected => {
-		interaction.editReply(':x: You took time to reply, giveaway was cancelled!');
-    msg.reactions.removeAll()
-	});
-  }  
-
+      })
+      .catch((collected) => {
+        interaction.editReply(
+          ":x: You took time to reply, giveaway was canceled!"
+        );
+        msg.reactions.removeAll();
+      });
+  },
 };
