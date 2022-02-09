52a545e553af9bfb1c608505b6b3ec7de2313e2e
Aim <91464959+Aim2339@users.noreply.github.com>
2022-02-09 10:58:31 +0530

    Format using Prettier

--- slash/invite.js
+++ slash/invite.js
@@ -1,62 +1,29 @@
 const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 
 module.exports = {
-  name: "links",
-  description: "Get a list of links related to the bot",
+  name: "invite",
+  description: "➕ Invite the bot to your server!",
   run: async (client, interaction) => {
     const row = new MessageActionRow().addComponents(
       new MessageButton()
-        .setCustomId("important")
-        .setLabel("Important:")
-        .setStyle("PRIMARY")
-        .setDisabled(true),
-
-      new MessageButton()
-        .setLabel(`Invite Link`)
+        .setLabel(`Invite ${client.user.username}`)
         .setStyle("LINK")
         .setURL(
-          `https://discord.com/api/oauth2/authorize?client_id=900628889452314674&permissions=8&scope=applications.commands%20bot`
-        ),
-
-      new MessageButton()
-        .setLabel(`Vote Link`)
-        .setStyle("LINK")
-        .setURL(`https://top.gg/bot/${client.user.id}/vote`)
+          `https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=applications.commands%20bot`
+        )
     );
-    const row2 = new MessageActionRow().addComponents(
-      new MessageButton()
-        .setCustomId("info")
-        .setLabel("Info:")
-        .setStyle("SUCCESS")
-        .setDisabled(true),
-
-      new MessageButton()
-        .setLabel(`Status Link`)
-        .setStyle("LINK")
-        .setURL(`https://stats.uptimerobot.com/8gMWRsXP3N/`),
-
-      new MessageButton()
-        .setLabel(`Website Link`)
-        .setStyle("LINK")
-        .setURL(`https://aim2339.github.io/GiveawayS/`)
-    );
-
-    const row3 = new MessageActionRow().addComponents(
-      new MessageButton()
-        .setCustomId("other")
-        .setLabel("Other:")
-        .setStyle("DANGER")
-        .setDisabled(true),
-
-      new MessageButton()
-        .setLabel(`Repository Link`)
-        .setStyle("LINK")
-        .setURL(`https://github.com/Aim2339/GiveawayS/`)
-    );
-
-    interaction.reply({
-      content: "Here are all the links related to the bot!",
-      components: [row, row2, row3],
-    });
+    let invite = new MessageEmbed()
+      .setAuthor(`Invite ${client.user.username} `, client.user.avatarURL())
+      .setTitle("Invite Link!")
+      .setDescription(
+        `Invite ${client.user} to your server & enjoy seamless giveaways with advanced features totally for free!`
+      )
+      .setColor("RANDOM")
+      .setTimestamp()
+      .setFooter(
+        `Requested by ${interaction.user.username} | GiveawaySforLife`,
+        interaction.user.displayAvatarURL()
+      );
+    interaction.reply({ embeds: [invite], components: [row] });
   },
 };
