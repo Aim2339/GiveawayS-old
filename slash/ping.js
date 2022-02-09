4369f9d7b9d66d755f5908f2a253b085959ce614
Aim <91464959+Aim2339@users.noreply.github.com>
2022-02-09 10:45:54 +0530

    Format using Prettier

--- slash/ping.js
+++ slash/ping.js
@@ -1,18 +1,21 @@
-const { MessageEmbed } = require("discord.js")
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-    name: 'ping',
-    description: '🏓Check my ping!',
-    run: async (client, interaction) => {
-      let pembed = new MessageEmbed()
-		  .setColor('RANDOM')	
-			.setTitle('Client Ping')
-			.addField('**Latency**', `\`${Date.now() - interaction.createdTimestamp}ms\``)
-			.addField('**API Latency**', `\`${Math.round(client.ws.ping)}ms\``)
-			.setTimestamp()
-			.setFooter(`${interaction.user.username}`, interaction.user.avatarURL());
-        interaction.reply({
-          embeds: [pembed]
-        });
-    },
+  name: "ping",
+  description: "🏓Check my ping!",
+  run: async (client, interaction) => {
+    let pembed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setTitle("Client Ping")
+      .addField(
+        "**Latency**",
+        `\`${Date.now() - interaction.createdTimestamp}ms\``
+      )
+      .addField("**API Latency**", `\`${Math.round(client.ws.ping)}ms\``)
+      .setTimestamp()
+      .setFooter(`${interaction.user.username}`, interaction.user.avatarURL());
+    interaction.reply({
+      embeds: [pembed],
+    });
+  },
 };
